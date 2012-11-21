ce0b393cde076e6264ba9cd208d2e3a142fe2e96
Pacorro <tyckogt@gmail.com>
2012-11-21 16:28:33 -0700

    Mis Fracciones

--- FraccionesEjemplo/main.m
+++ FraccionesEjemplo/main.m
@@ -8,13 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+//Seccion @Interface
+
+@interface Fraccion : NSObject{
+    int numerador;
+    int denominador;
+}
+
+-(void) imprimir;
+-(void) setNumerador: (int) n;
+-(void) setDenominador: (int) d;
+
+@end
+
+//seccion de implementacion
+@implementation Fraccion
+-(void)imprimir{
+    float decimales;
+    decimales=(float) numerador/denominador;
+    NSLog(@"%2f",decimales);
+    NSLog(@"%i/%i",numerador,denominador);
+}
+
+-(void) setNumerador:(int)n{
+    numerador=n;
+}
+-(void) setDenominador:(int)d{
+    denominador=d;
+}
+@end
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        Fraccion *miFraccion;
+        //Crear una instancia de la fraccion
+        miFraccion=[Fraccion alloc];
+        miFraccion=[miFraccion init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //Asignacion de valores de la fraccion
+        [miFraccion setNumerador:45];
+        [miFraccion setDenominador:46];
+        
+        //resultado
+        NSLog(@"El resultado es:");
+        [miFraccion imprimir];
+        
         
     }
     return 0;
